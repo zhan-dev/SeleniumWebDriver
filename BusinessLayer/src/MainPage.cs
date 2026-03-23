92d422212ac9ec637be3027d419e0a0514a292f5
Zhan Kariahin <Zhan.Kariahin@gmail.com>
2026-03-23 12:07:29 +0200

    Main Page Test

--- BusinessLayer/src/MainPage.cs
+++ BusinessLayer/src/MainPage.cs
@@ -1,5 +1,6 @@
 ﻿using OpenQA.Selenium;
 using OpenQA.Selenium.Support.UI;
+using System.Xml.Linq;
 
 namespace BusinessLayer.src
 {
@@ -9,11 +10,21 @@ namespace BusinessLayer.src
 
         private readonly string url = "https://www.epam.com/";
 
-        private readonly By title = By.TagName("title");
+        private readonly By titleBy = By.TagName("title");
+        private readonly By searchButtonBy = By.ClassName("header__icon");
+        private readonly By headerSearchPanelBy = By.ClassName("header-search__panel");
+        private readonly By searchInputBy = By.Id("new_form_search");
+        private readonly By findButtonBy = By.CssSelector(".search-results__action-section > button");
+        private readonly By searchResultsCollectionElementsBy = By.ClassName("search-results__item");
 
         public MainPage(IWebDriver driver)
         {
             this.driver = driver;
+        }
+
+        public void MaximizeWindow()
+        {
+            this.driver.Manage().Window.Maximize();
         }
 
         public void GoToUrl()
@@ -28,8 +39,49 @@ namespace BusinessLayer.src
 
         public IWebElement WaitUntilTitleIsPresented()
         {
-            var titleWait = new WebDriverWait(driver, TimeSpan.FromSeconds(5));
-            return titleWait.Until(driver => driver.FindElement(this.title));
+            var titleWait = new WebDriverWait(this.driver, TimeSpan.FromSeconds(5));
+            return titleWait.Until(driver => driver.FindElement(this.titleBy));
+        }
+
+        public void SearchButtonClick()
+        {
+            this.driver.FindElement(this.searchButtonBy).Click();
+        }
+
+        public void InputDataIntoSearchInput(string searchText)
+        {
+            var searchPanel = this.driver.FindElement(headerSearchPanelBy);
+
+            var waitInput = new WebDriverWait(this.driver, TimeSpan.FromSeconds(5));
+
+            var activeInput = waitInput.Until(drv =>
+            {
+                var element = searchPanel.FindElement(searchInputBy);
+                return (element.Displayed && element.Enabled) ? element : null;
+            });
+
+            activeInput.Click();
+            activeInput.Clear();
+            activeInput.SendKeys(searchText);
+        }
+
+        public void FindClick()
+        {
+            var searchPanel = this.driver.FindElement(headerSearchPanelBy);
+            var findButton = searchPanel.FindElement(findButtonBy);
+            findButton.Click();
+        }
+
+        public IReadOnlyCollection<IWebElement> GetSearchResultCollection()
+        {
+            var containerWait = new WebDriverWait(this.driver, TimeSpan.FromSeconds(5));
+
+            return containerWait.Until(drv =>
+            {
+                var elements = drv.FindElements(searchResultsCollectionElementsBy);
+                return elements.Count > 0 ? elements : null;
+            });
+
         }
     }
 }
--- EPAM.Tests/src/MainPageTests.cs
+++ EPAM.Tests/src/MainPageTests.cs
@@ -12,24 +12,57 @@ namespace EPAM.Tests.src
     {
         private MainPage mainPage;
 
+        private static readonly string[] searchCorrectKeywords = ["BLOCKCHAIN", "Cloud", "Automation"];
+
         public override void SetUp()
         {
             ChromeDriverFactory driverFactory = new ChromeDriverFactory();
             this.driver = driverFactory.CreateDriver(WebBrowserMode.UXUI);
 
             this.mainPage = new MainPage(this.driver);
-            mainPage.GoToUrl();
-            mainPage.WaitUntilTitleIsPresented();
+            this.mainPage.MaximizeWindow();
+            this.mainPage.GoToUrl();
+            this.mainPage.WaitUntilTitleIsPresented();
         }
 
         [Test]
-        public void UserGoToUrl_UserWaitUntilTitleIsLoaded_TitleIsAsExpected()
+        public void UserGoToUrl_WaitUntilTitleIsLoaded_TitleIsAsExpected()
         {
             //Arrange
             string expectedTitle = "EPAM | Software Engineering & Product Development Services";
 
             // Assert
-            Assert.That(mainPage.GetTitle(), Is.EqualTo(expectedTitle));
+            Assert.That(this.mainPage.GetTitle(), Is.EqualTo(expectedTitle));
+        }
+
+        [TestCaseSource(nameof(searchCorrectKeywords))]
+        public void UserGoToGlobalSearch_UseGlobalSearchPanel_SearchResultsIsAsExpected(string searchText)
+        {
+            //Act
+            this.mainPage.SearchButtonClick();
+            this.mainPage.InputDataIntoSearchInput(searchText);
+            this.mainPage.FindClick();
+
+            var results = this.mainPage.GetSearchResultCollection();
+
+            // Assert
+            Assert.That(results, Is.Not.Empty);
+
+            foreach (var result in results)
+            {
+                var title = result.FindElement(By.CssSelector(".search-results__title-link")).Text;
+                var link = result.FindElement(By.CssSelector(".search-results__title-link")).GetAttribute("href");
+                var description = result.FindElement(By.CssSelector(".search-results__description")).Text;
+
+                Console.WriteLine($"{title} -> {link}");
+                Console.WriteLine(description);
+            }
+
+            bool allValid = results.All(key =>
+                searchText.Any(keyword =>
+                    key.Text.Contains(keyword, StringComparison.OrdinalIgnoreCase)));
+
+            Assert.That(allValid, Is.True);
         }
 
         [TearDown]
