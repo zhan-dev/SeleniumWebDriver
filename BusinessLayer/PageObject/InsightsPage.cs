84401469f51623da9b497474483bd4ac65675b89
Zhan Kariahin <Zhan.Kariahin@gmail.com>
2026-04-05 21:22:59 +0300

    InsightsPage improvements

--- BusinessLayer/PageObject/InsightsPage.cs
+++ BusinessLayer/PageObject/InsightsPage.cs
@@ -9,11 +9,14 @@ namespace BusinessLayer.PageObject
         private readonly IWebDriver driver;
 
         private readonly By titleBy = By.TagName("title");
+        
         private readonly By mainSliderRightArrowButtonBy = 
             By.XPath("//div[@class='slider section']//div[@class='slider__navigation']//button[contains(@class,'slider__right-arrow')]");
+        
         private readonly By mainSliderBusinessValueTextBy =
             By.XPath("//div[@class='slider section']//div[contains(@class, 'single-slide__content')]//div[@class='text']" +
                 "//p//span[contains(@class, 'museo-sans-light') or contains(@class, 'gradient-text')]");
+        
         private readonly By readTheReportButtonBy = By.XPath("//a[contains(@href, 'ai-report-2025') and @tabindex='0']");
         private readonly By introductionHeaderBy = By.TagName("h1");
 
@@ -45,20 +48,11 @@ namespace BusinessLayer.PageObject
             return string.Join("", spans.Select(s => s.Text)).Trim();
         }
 
-        //public string MainSliderGetBusinessValueText()
-        //{
-        //    var spans = this.driver.FindElements(mainSliderBusinessValueTextBy);
-
-        //    return string.Join("", spans.Select(s => s.Text)).Trim();
-        //}
-
         public string IntroductionHeaderText()
         {
             WaitUntilTitleIsPresented();
 
-            var headerText = this.driver.FindElement(introductionHeaderBy).Text;
-
-            return headerText;
+            return this.driver.FindElement(introductionHeaderBy).Text;
         }
 
         private void WaitUntilTitleIsPresented()
