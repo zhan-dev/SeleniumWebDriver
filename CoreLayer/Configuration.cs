649a4c9e2a1365a1154c06a0a0653c275f9fae5a
Zhan Kariahin <Zhan.Kariahin@gmail.com>
2026-05-07 17:10:16 +0300

    Add CI pipeline

--- CoreLayer/Configuration.cs
+++ CoreLayer/Configuration.cs
@@ -14,12 +14,17 @@ public static class Configuration
 
     public static void Init()
     {
+        string? browserEnv = Environment.GetEnvironmentVariable("BROWSER");
+
         var configuration = new ConfigurationBuilder()
             .SetBasePath(Directory.GetCurrentDirectory())
             .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
             .Build();
 
-        BrowserType = configuration["Application:BrowserType"] ?? BrowserType;
+        BrowserType = !string.IsNullOrWhiteSpace(browserEnv)
+        ? browserEnv
+        : configuration["Application:BrowserType"] ?? BrowserType;
+        Console.WriteLine($"[DEBUG] Browser from env: {browserEnv}");
         AppUrl = configuration["Application:ApplicationUrl"] ?? AppUrl;
         TestDataPath = configuration["Application:TestDataPath"] ?? TestDataPath;
         LoggingLevel = configuration["Logging:MinimumLevel:Default"] ?? LoggingLevel;
